840dbfccc0e49fb2a44f9dc98391884ec9bab092
lsw6684 <lsw6684@hanmail.net>
2021-07-01 23:12:14 +0900

    business logic

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,15 +3,21 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
-    private final MemberRepository memberRepositor;
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
     public MemberService(MemberRepository memberRepositor) {
-        this.memberRepositor = memberRepositor;
+        this.memberRepository = memberRepositor;
     }
 
 
