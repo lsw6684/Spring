bb0c0c7f05d44a930726b246b99992f8443ce799
lsw6684 <lsw6684@hanmail.net>
2022-05-08 15:07:18 +0900

    DIP 위반, 클라이언트 코드에서 구체 클래스에도 의존

--- core/core/src/test/java/hello/core/order/OrderServiceTest.java
+++ core/core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,2 +1,27 @@
-package hello.core.order;public class OrderServiceTest {
+package hello.core.order;
+
+import hello.core.Order.Order;
+import hello.core.Order.OrderService;
+import hello.core.Order.OrderServiceImpl;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId =  1L;    // long 으로 쓰면 null이 불가능 하여 Long으로 함.
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
 }
