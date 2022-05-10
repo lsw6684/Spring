b9060dd80c2d5f6afb06ae5c6a51f21750f49123
lsw6684 <lsw6684@hanmail.net>
2022-05-11 00:24:51 +0900

    Spring Container

--- core/core/src/main/java/hello/core/Order/AppConfig.java
+++ core/core/src/main/java/hello/core/Order/AppConfig.java
@@ -6,17 +6,24 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 memberRepository()
@@ -24,6 +31,7 @@ public class AppConfig {
         );
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
