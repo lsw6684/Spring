40d0d28986161429d2dd51ae4c65c7e3a0762dfa
lsw6684 <lsw6684@hanmail.net>
2022-11-16 22:19:17 +0900

    frontController

--- servlet/servlet/src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberFormControllerV4.java
+++ servlet/servlet/src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberFormControllerV4.java
@@ -6,7 +6,8 @@ import java.util.Map;
 
 public class MemberFormControllerV4 implements ControllerV4 {
     @Override
-    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+    public String process(Map<String, String> paramMap, Map<String, Object>
+            model) {
         return "new-form";
     }
 }
--- servlet/servlet/src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberListControllerV4.java
+++ servlet/servlet/src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberListControllerV4.java
@@ -8,13 +8,12 @@ import java.util.List;
 import java.util.Map;
 
 public class MemberListControllerV4 implements ControllerV4 {
-
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
-    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+    public String process(Map<String, String> paramMap, Map<String, Object>
+            model) {
         List<Member> members = memberRepository.findAll();
-
         model.put("members", members);
         return "members";
     }
